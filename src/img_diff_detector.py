212f310254c8447fa0213e16e807d7294f4ef1c5
shuhei0916 <shuheiito.stc@gmail.com>
2024-09-14 21:52:57 +0900

    Modify calc_diff

--- src/img_diff_detector.py
+++ src/img_diff_detector.py
@@ -1,2 +1,21 @@
-def calc_diff(img1, img2):
-    return 0
+import cv2
+import numpy as np
+
+def calc_diff(img1_path, img2_path):
+    img1 = cv2.imread(img1_path)
+    img2 = cv2.imread(img2_path)
+
+    im_diff = img1.astype(int) - img2.astype(int)
+    im_diff_abs = np.abs(im_diff)
+    print(f'{im_diff_abs=}')
+    
+    return im_diff_abs
+
+
+def main():
+    im= cv2.imread('data/saizeriya1.jpg')
+    print(im.shape)
+    print(im.dtype)
+
+if __name__ == '__main__':
+    main()
