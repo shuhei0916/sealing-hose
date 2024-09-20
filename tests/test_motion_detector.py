6594b9cbfed43c119b581180683931b32d9f7f89
shuhei0916 <shuheiito.stc@gmail.com>
2024-09-20 12:53:50 +0900

    Add test_get_video_properties() in test_motion_detector

--- tests/test_motion_detector.py
+++ tests/test_motion_detector.py
@@ -1,13 +1,15 @@
 import unittest
+from unittest.mock import Mock
 import numpy as np
 import cv2
+from src import motion_detector as md
 
 class TestAbsDiff(unittest.TestCase):
     def setUp(self):
         self.img1 = cv2.imread('data/lena.jpg')
         self.img2 = cv2.imread('data/lena_q25.jpg')
-        self.assertIsNotNone(self.img1, "Failed to load image from 'data/lena.jpg'")
-        self.assertIsNotNone(self.img2, "Failed to load image from 'data/lena_q25.jpg'")
+        self.assertIsNotNone(self.img1, 'Failed to load image from "data/lena.jpg"')
+        self.assertIsNotNone(self.img2, 'Failed to load image from "data/lena_q25.jpg"')
         
     def test_identical_images(self):
         actual = cv2.absdiff(self.img1, self.img1)
@@ -19,5 +21,17 @@ class TestAbsDiff(unittest.TestCase):
         self.assertTrue(np.any(img_diff > 0), 'Expected the two images to have pixel differences.')
 
 
+class TestMotionDetector(unittest.TestCase):
+    def test_get_video_properties(self):
+        cap = Mock()
+        cap.get.side_effect = [30.0, 1920, 1080]
+
+        fps, frame_width, frame_height = md.get_video_properties(cap)
+
+        self.assertEqual(fps, 30.0)
+        self.assertEqual(frame_width, 1920)
+        self.assertEqual(frame_height, 1080)
+
+        
 if __name__ == '__main__':
     unittest.main()
