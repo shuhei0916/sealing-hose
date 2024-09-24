9f55b34fc0a74e123f9ea2129e60023390133786
shuhei0916 <shuheiito.stc@gmail.com>
2024-09-24 14:55:55 +0900

    Add threshlding and noise reduction process

--- src/motion_detector.py
+++ src/motion_detector.py
@@ -11,12 +11,12 @@ def get_video_properties(cap):
 
 def main():
     video_path = 'data/vtest_10sec.mp4'
-    output_path = 'data/dst/out_10sec.mp4'
+    output_path = 'data/dst/thresh_10sec.mp4'
     
     cap = cv2.VideoCapture(video_path)
     
     fps, frame_width, frame_height = get_video_properties(cap)    
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height), isColor=False)
 
     ret, frame1 = cap.read()
@@ -30,8 +30,16 @@ def main():
         frame2_gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
         diff = cv2.absdiff(frame1_gray, frame2_gray)
         
-        cv2.imshow('Motion Detection', diff)
-        out.write(diff)
+        
+        # 動きのある領域を強調するために閾値処理を適用
+        _, thresh = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)
+
+        # 差分を膨張させて小さなノイズを除去（モルフォロジー変換）
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+        dilated = cv2.dilate(thresh, kernel, iterations=2) 
+        
+        cv2.imshow('Motion Detection', thresh)
+        out.write(thresh)
         
         frame1_gray = frame2_gray
         if cv2.waitKey(1) & 0xFF == ord('q'):
