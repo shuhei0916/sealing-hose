0e586db5565eaf104c9935a54d3a8f37079027f4
shuhei0916 <shuheiito.stc@gmail.com>
2024-10-14 01:49:12 +0900

    Organize some variable names

--- src/motion_detector.py
+++ src/motion_detector.py
@@ -2,11 +2,17 @@ import cv2
 import numpy as np
 from common import get_video_properties, draw_contours
 
-
-def main():
-    video_path = 'data/vtest.avi'
-    output_path = 'data/dst/vtest_processed.mp4'
+def find_contours(motion_diff, threshold_value=90):
+    _, thresh = cv2.threshold(motion_diff, threshold_value, 255, cv2.THRESH_BINARY)
     
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+    dilated = cv2.dilate(thresh, kernel, iterations=2)
+    
+    motion_contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    return motion_contours
+
+def detect_motion(video_path, output_path):
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
         print("Error: Could not open video.")
@@ -15,34 +21,29 @@ def main():
     fps, frame_width, frame_height = get_video_properties(cap)
     
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height), isColor=True)
+    output_video_writer = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height), isColor=True)
 
-    ret, frame1 = cap.read()
+    ret, prev_frame = cap.read()
     if not ret:
         print("Error: Could not read video frame.")
         return
-    frame1_gray = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    frame1_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
 
     while cap.isOpened():
-        ret, frame2 = cap.read()
+        ret, current_frame = cap.read()
         if not ret:
             break
 
-        frame2_gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+        frame2_gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
         
-        diff = cv2.absdiff(frame1_gray, frame2_gray)
+        motion_diff = cv2.absdiff(frame1_gray, frame2_gray)
         
-        _, thresh = cv2.threshold(diff, 90, 255, cv2.THRESH_BINARY)
-
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
-        dilated = cv2.dilate(thresh, kernel, iterations=2)
+        motion_contours = find_contours(motion_diff)
         
-        contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        frame_with_contours = draw_contours(frame2.copy(), contours)
+        frame_with_contours = draw_contours(current_frame.copy(), motion_contours)
         
         cv2.imshow('Motion Detection', frame_with_contours)
-        out.write(frame_with_contours)
+        output_video_writer.write(frame_with_contours)
 
         frame1_gray = frame2_gray
         
@@ -50,9 +51,15 @@ def main():
             break
 
     cap.release()
-    out.release()
+    output_video_writer.release()
     cv2.destroyAllWindows()
 
+
+def main():
+    video_path = 'data/vtest.avi'
+    output_path = 'data/dst/vtest_processed.mp4'
+    
+    detect_motion(video_path, output_path)
 
 if __name__ == '__main__':
     main()
