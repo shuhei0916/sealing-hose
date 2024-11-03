db311b9d03e1c3721b01359517ad70030582413b
shuhei0916 <shuheiito.stc@gmail.com>
2024-11-03 23:26:32 +0900

    Modify hello.py to test validate_timestamp()

--- src/hello.py
+++ src/hello.py
@@ -1,1 +1,14 @@
-print("hello")
+from common import validate_system_coherence, validate_timestamp
+
+
+def print_hello():
+    print("hello")
+    
+
+def main():
+    validate_system_coherence()
+    print_hello()
+    
+
+if __name__ == '__main__':
+    main()
